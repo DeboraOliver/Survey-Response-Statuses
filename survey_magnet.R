35b24c78f8fb466528204908262de7cfe74c4edc
Debora Office <debora.ufabc@gmail.com>
2019-02-08 08:03:27 -0200

    First Commit

---
+++ survey_magnet.R
@@ -0,0 +1,44 @@
+library(redcapAPI)
+library(tidyr)
+library(ggplot2)
+library(dplyr)
+
+source("token.txt")
+
+rcon <- redcapConnection(url=url, token=token)
+
+rm(token)
+
+magnet <- exportRecords(rcon,  factors = FALSE,
+                               labels = FALSE, dates = TRUE,
+                               events = NULL)
+
+clean_magnet<- transform(magnet, 
+                        Date = substr(magnet[,3], 1,10), 
+                        Hour = substr(magnet[,3], 12,20))
+
+dates_responses <- select(clean_magnet, record_id, Date)
+ 
+na.omit(dates_responses$Date)
+
+
+summary_results <- summary(dates_responses$Date)
+
+daily_responses <- data.frame()
+
+daily_responses <- data.frame(summary_results)
+daily_responses$days <- 1:nrow(daily_responses)
+
+
+
+  
+  #aggregate(Date_demografics~record_id, data = clean_magnet, sum)
+  
+
+
+ ggplot(daily_responses, aes(days,summary_results)) +
+   geom_point() +
+   geom_smooth(method="loess", na.rm=TRUE, se=FALSE, show.legend = TRUE, span = 0.25) 
+ 
+
+  
